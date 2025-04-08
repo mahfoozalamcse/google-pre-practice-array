660fbffdcab5e2d4168e79b280b4508c991e1b54
mahfoozalamcse <amahfooz1007@gmail.com>
2025-04-08 16:34:38 +0530

    practice search insert operation

--- OperationArray.java
+++ OperationArray.java
@@ -1,6 +1,60 @@
 public class OperationArray {
+    // insert opration
+    public static void insert(int arr[], int x){
+      arr[arr.length-1] = x; 
+    }
+
+    // insert at given position
+    public static void insertPos(int arr[], int x, int pos, int currSize, int capcity){
+        if (currSize > capcity) {
+            return;
+        }
+        if (pos > capcity) {
+            return;
+        }
+        int idx = pos -1;
+
+        for (int i=currSize-1; i >=idx; i--) {
+            arr[i+1] = arr[i];
+        }
+
+        arr[idx] = x;
+
+    }
+
+    // serch in array
+    public static void serch(int arr[], int x){
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                System.out.print(arr[i]+" ");
+                return;
+            }
+        }
+    }
+
+    // print array
+    public static void printArray(int arr[]){
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]+" ");
+        }
+    }
 
     public static void main(String[] args) {
-        
+      int arr[] = new int[5];
+      arr[0] = 12;
+      arr[2] = 34;
+      arr[1] = 24;
+      arr[3] = 45;
+
+      int currSize = 4;
+      int capcity = 5;
+      int x = 34;
+      int pos = 2;
+      //insert(arr, x);
+      //printArray(arr);
+      insertPos(arr, x, pos, currSize, capcity);
+      printArray(arr);
+
+      serch(arr, 34);
     }
 }
