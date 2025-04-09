14280a014e5badb578a0f0857b2f1992a35b68c7
mahfoozalamcse <amahfooz1007@gmail.com>
2025-04-09 21:33:48 +0530

    method 2 solve rotation problem solved

--- LeftRotate.java
+++ LeftRotate.java
@@ -35,8 +35,23 @@ public class LeftRotate {
 
     }
 
-    // efficient
-    public static void rotation
+    // method 2
+    public static void reverse(int arr[], int start, int end){
+      while (start < end) {
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+        start++;
+        end--;
+      }
+    }
+    public static void rotation2(int arr[], int d){
+        reverse(arr, 0, d-1);
+        reverse(arr, d, arr.length-1);
+        reverse(arr, 0, arr.length-1);
+    }
+
+    
 
     // print array
     public static void printArray(int arr[]){
@@ -48,7 +63,8 @@ public class LeftRotate {
   public static void main(String[] args) {
    int arr[] = {1, 2, 4, 9};
   // leftRotateOne(arr);
-    rotateByD(arr, 2);
+   // rotateByD(arr, 2);
+   rotation2(arr, 2);
    printArray(arr);
   }
 }
