a5c866fc36e68fcc459cc5a98bc981e471073ecc
mahfoozalamcse <amahfooz1007@gmail.com>
2025-04-09 17:25:00 +0530

    check sorted or not

--- MaxElement.java
+++ MaxElement.java
@@ -10,13 +10,23 @@ public class MaxElement {
         return max;
     }
 
+    public static int checkSortedorNot(int arr[]){
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i-1] > arr[i] ) {
+                return -1;
+            }
+        }
+        return 1;
+    }
+
     public static void printArray(int arr[]){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]+" ");
         }
     }
     public static void main(String[] args) {
-      int arr[] = {12, 89, 99, 90, 76};
+      int arr[] = {12, 99, 90, 90, 96};
       System.out.println(max(arr)); 
+      System.out.println(checkSortedorNot(arr));
     }
 }
